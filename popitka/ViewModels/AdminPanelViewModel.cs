9bc990ab62aa178fbebccfe9f6a095fab07a549d
Bunker <Bunker@DESKTOP-ET3V0BN>
2025-05-14 19:46:46 +0300

    добавил админ панель. админ может добавлять удалять и  редактировать продукты. пользователь так же видит продукты. теперь страничка продукта генерирует данные из бд.

---
+++ popitka/ViewModels/AdminPanelViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using YourProject.Domain.Models;
+
+namespace popitka.ViewModels
+{
+    public class AdminPanelViewModel
+    {
+        public List<User> Users { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
